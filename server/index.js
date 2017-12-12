b8a0829ce105aa913cf1f3ba072564c6adafad69
Rory Well <rory.e.well@gmail.com>
2017-12-12 02:04:48 -0600

    Stub out the crypto selector list route on the back-end

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
+const cryptoSelectorList = require('./routes/cryptoSelectorList/index.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use('/crypto-selector-list', cryptoSelectorList);
+// app.use('/crypto', crypto);
+
 app.listen(5123);
