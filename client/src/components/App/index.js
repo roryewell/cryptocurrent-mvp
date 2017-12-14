31860b059cdd178eccb550bfc17b76d14f231331
Rory Well <rory.e.well@gmail.com>
2017-12-13 19:27:02 -0600

    Set up cryptoSelectorList route on initial page load, now receiving list

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,13 +2,31 @@ import React from 'react';
 import { render } from 'react-dom';
 import CryptoSelectorList from '../CryptoSelectorList/index.js';
 import DashboardCryptoList from '../DashboardCryptoList/index.js';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cryptoSelectorList: ['rory', 'joe', 'pryce']
+      cryptoSelectorList: []
     }
+  }
+  componentDidMount() {
+    this.getCryptoSelectorList();
+  }
+  getCryptoSelectorList() {
+    axios.get('http://localhost:5123/crypto-selector-list')
+      .then((response) => {
+        const cryptoList = response.data.map((crypto) => {
+          return crypto.id;
+        });
+        this.setState({
+          cryptoSelectorList: cryptoList
+        });
+      })
+      .catch((err) => {
+        console.log('Error fetching crypto list: ', err);
+      });
   }
   render() {
     return (
--- server/routes/cryptoSelectorList/index.js
+++ server/routes/cryptoSelectorList/index.js
@@ -4,8 +4,14 @@ const getCyrptoStats = require('../../lib/helpers.js');
 const Crypto = require('../../models/Crypto/index.js');
 
 router.get('/', (req, res) => {
-  res.send('Testing sir');
-
+  Crypto
+    .find()
+    .limit(10)
+    .sort({ rank: 1 })
+    .select({ id: 1 })
+    .then((docs) => {
+      res.send(docs);
+    })
 })
 
 module.exports = router;
